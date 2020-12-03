05c00e5c087dd2ce548f131dfd64b53a8dde5289
LunixiaLIVE <lunixia@live.com>
2020-12-02 23:07:03 -0600

    Update SmexyMailCore.lua

--- SmexyMailCore.lua
+++ SmexyMailCore.lua
@@ -8,42 +8,21 @@ if SmexyMailDB == nil then
 	SmexyMailDB = {}; 
 end;
 
-SmexyMailBar = CreateFrame("Frame",SmexyMailBar,UIParent, BackdropTemplateMixin and "BackdropTemplate");
-
-function SmexyMail:SmexyMailUpdate()
-	--print("Do things with mailbox");
-	SmexyMailSideBar = CreateFrame("Frame",SmexyMailSideBar,SmexyMailBar);
-	local f = SmexyMailBar
-	f:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
-			edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
-			tile = true, tileSize = 16, edgeSize = 16, 
-			insets = { left = 1, right = 1, top = 1, bottom = 1 }}) 
-	f:SetBackdropColor(0,0,0,1)
-	f:SetFrameStrata("TOOLTIP")
-	
-	--f:SetWidth(MailFrame:GetWidth()-32) 
-	--f:SetHeight(50)
-	
-	f:SetHeight(MailFrame:GetHeight());
-	f:SetWidth(128);
-	
-	SmexyMailSideBartxt = f:CreateFontString(nil,"OVERLAY","GameFontWhite")
-	local ft = SmexyMailSideBartxt 
-	ft:ClearAllPoints()
-	ft:SetAllPoints(SmexyMailBar) 
-	ft:SetJustifyH("CENTER")
-	ft:SetJustifyV("CENTER")
-	f:ClearAllPoints()
-	f:SetPoint("RIGHT",MailFrame,"RIGHT",128,0)
-	--f:SetPoint("CENTER",MailFrame,"CENTER",0,32)
-	f:SetParent(MailFrame)
-	f:Show()            
+function SmexyMailUpdate()
+	print("Make Buttons")
+	local btn_Tailor = SmexyMail:GenerateButton("Tailor")
+	btn_Tailor:SetScript("OnClick", function(self) print("Send to Tailor") end)
+	btn_Tailor:Show()
 end;
 
-local f = SmexyMailBar
-f:RegisterEvent("MAIL_SHOW") 
-f:SetScript("OnEvent", function(self, event, ...)
-	if event == "MAIL_SHOW" then
-		SmexyMail:SmexyMailUpdate();
-	end;
-end)
+function SmexyMail:GenerateButton(buttonName)
+	local button = CreateFrame("button", buttonName, MailFrame, "UIPanelButtonTemplate")
+	button:SetHeight(25)
+	button:SetWidth(100)
+	button:SetPoint("TOPRIGHT", MailFrame, "TOPRIGHT", 100, 0)
+	button:SetText(buttonName)
+	button:SetAlpha(1)
+	return button
+end;
+
+MailFrame:HookScript("OnShow", SmexyMailUpdate)
